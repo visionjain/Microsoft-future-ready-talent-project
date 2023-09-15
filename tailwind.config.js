a5a6efec0af730c765035511d1385bce7b3f8a2c
Vision Jain <visionjain118@gmail.com>
2023-09-16 03:02:03 +0530

    sdfshfcgs

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+  future: {},
+  purge: [],
+  theme: {
+  extend: {},
+  },
+  variants: {},
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
+}
+  
